9872e3943eb23504b0fa3ae32fdcc910f1071593
Sutanu Sain <sutanu.sain@gmail.com>
2021-06-06 20:42:32 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,35 @@
 import React, { Component, useState, useEffect } from "react";
 import '../styles/App.css';
-
-const App = () => {
-  // write your code here 
-
+const App = ({hoursMinsecs}) =>{
+  const {hours =0, minutes =0, seconds = 60} = hoursMinsecs;
+  const [[hrs, mins, sec], setTime] = React.useState([hours, minutes, seconds]);
+const  tick= () => {
+  if(hrs === 0 && mins === 0 && secs === 0)
+    reset()
+  else if (mins === 0 && secs === 0){
+   setTime([hrs -1, 59, 59]);
+  } else if ( secs === 0){
+  setTime([hrs, mins -1, 59])
+  } else {
+   setTime([hrs, mins, secs-1]);
+  }
+};
+  const reset =() => setTime([parseInt(hours), parseInt(minutes), parseInt(seconds)]);
+  
+  React.useEffect(() =>{
+    const timerId = setInterval(() => tick(), 1000);
+    return () => clearInterval(timerId);
+  });
+  
+  function counter(){
+    
+  }
+  
   return (
     <div className="wrapper">
       <div id="whole-center">
         <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={/* callback function */} /> sec.
+          Reverse countdown for<input id="timeCount" onKeyDown={this.counter}> sec.
         </h1>
       </div>
       <div id="current-time">{/* remaining time */}</div>
